58d40fa34fc8ad92bfd5a60d1a6fa15b16f61e90
root <admin@9isuper.com>
2017-07-15 15:33:25 +0800

    add eureka

--- 9isuper-config-server/src/main/java/com/isuper/cloud/config/Application.java
+++ 9isuper-config-server/src/main/java/com/isuper/cloud/config/Application.java
@@ -3,9 +3,11 @@ package com.isuper.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
