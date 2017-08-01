463c0887c14649bf79f43bcafa5063bfe02f9752
邹永乐 <yongle.zou@bl.com>
2017-08-01 10:05:39 +0800

    排除了静态资源文件的过滤

--- 9isuper-config-server/src/main/java/com/isuper/cloud/Application.java
+++ 9isuper-config-server/src/main/java/com/isuper/cloud/Application.java
@@ -1,4 +1,4 @@
-package com.isuper.cloud.config;
+package com.isuper.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
